51945dad5f23a343da420dcb9eb7d1711e3470b8
Victor Díez Serrano <04.diez.v@gmail.com>
2025-11-26 10:04:02 +0100

    contador de nivel

--- Assets/Scripts/WinTileS.cs
+++ Assets/Scripts/WinTileS.cs
@@ -1,18 +1,39 @@
-using UnityEditor.SearchService;
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class WinTileS : MonoBehaviour
 {
     public string nextLevelName = "Level2";
+    public float delayBeforeLoad = 1f;
+    public TMP_Text levelText;
+
+    private bool hasTriggered = false;
+
+    void Start()
+    {
+        if (levelText != null)
+        {
+            int currentLevel = SceneManager.GetActiveScene().buildIndex;
+            levelText.text = "Level: " + currentLevel ;
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !hasTriggered)
         {
-            Debug.Log("¡Game Over!");
-            Debug.Log("Siguiente nivel sería: " + nextLevelName);
-            SceneManager.LoadScene(nextLevelName);
+            hasTriggered = true;
+            Debug.Log("¡Victoria!");
+            Debug.Log("Cargando nivel: " + nextLevelName);
+            StartCoroutine(LoadNextLevelWithDelay());
         }
+    }
+
+    IEnumerator LoadNextLevelWithDelay()
+    {
+        yield return new WaitForSeconds(delayBeforeLoad);
+        SceneManager.LoadScene(nextLevelName);
     }
 }
